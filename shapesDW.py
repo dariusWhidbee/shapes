b3408f2afcaa2903baf09a2dda47dae1b359290b
csPython <cspython@AD25TF04NGF1J-78.local>
2019-02-26 15:37:06 -0500

    First shape

--- shapesDW.py
+++ shapesDW.py
@@ -3,4 +3,23 @@
 from graphics import*
 # Brings in all functions to my program.
 
-bTriWin = GraphWin("Blue Triange" 300, 300)
+winX = 600
+winY = 600
+window = GraphWin("Multiple Shapes", winX, winY)
+# Creates a title on the Top Border of the window.
+# As well as set the windows Width(550, 1st) and height(550, 2nd)
+window.setCoords(0, 0, winX, winY)
+# Sets coordinates
+
+# diamond
+diaSize = 70
+# diamonds size
+dia = Polygon(Point(winX/2 - diaSize, winY/2),
+                  Point(winX/2, winY/2 + diaSize),
+                  Point(winX/2 + diaSize, winY/2),
+                  Point(winX/2, winY/2 - diaSize))
+# diamonds points
+dia.draw(window)
+# draws the diamond
+dia.setFill(color_rgb(0,170,0))
+# color
